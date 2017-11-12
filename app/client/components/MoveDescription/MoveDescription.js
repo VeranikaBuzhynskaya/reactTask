e7a620aa62deaef66b6993eb7e99af6d8bd5d86f
VeranikaBuzhynskaya <fruittelllla@gmail.com>
2017-11-12 23:44:48 +0300

    Correct display of information depending on the type of film

--- app/client/components/MoveDescription/MoveDescription.js
+++ app/client/components/MoveDescription/MoveDescription.js
@@ -23,29 +23,57 @@ class MoveDescription extends React.Component{
         this.props.showUniqueFilm(id);
     }
 
+    infoOfFilm(info){
+        if(this.props.searchType === "movie"){
+            return {
+                title: info.title,
+                raiting: info.vote_average || '?',
+                release_date: info.release_date,
+                runtime: info.runtime,
+                overview: info.overview,
+                genres: info.genres ? info.genres[0].name : 'indefined',
+                production_countrie: info.production_countries
+                    ? info.production_countries[0].name
+                    : 'indefined'
+
+            };
+        }else{
+            return {
+                title: info.name,
+                raiting: info.vote_average || '?',
+                release_date: info.first_air_date,
+                runtime: info.episode_run_time
+                    ? info.episode_run_time[0]
+                    : 'indefined',
+                overview: info.overview,
+                genres: info.genres ? info.genres[0].name : 'indefined',
+                production_countrie: info.origin_country
+                    ? info.origin_country[0]
+                    : 'indefined'
+            };
+        }
+    }
+
     render(){
         const info = this.props.info;
-        console.log(info);
+        const infoFilm = this.infoOfFilm(info);
         return (
             <div className="move-description-part">
                 <img className="poster-image" src={info.poster_path ? 'https://image.tmdb.org/t/p/w500' +
                 info.poster_path : 'https://www.beddingwarehouse.com.au/wp-content/uploads/2016/01/placeholder-featured-image-600x600.png'}/>
                 <div className="description-part">
-                    <h2>{info.title || info.name}</h2>
-                    <p className="raiting">Raiting: <span className="raiting-average">{info.vote_average}</span></p>
+                    <h2>{infoFilm.title}</h2>
+                    <p className="raiting">Raiting: <span className="raiting-average">{infoFilm.raiting}</span></p>
                     <div className="runtime-and-release-year">
-                       <span>{info.release_date || info.first_air_date}</span>
-                       <span>{info.runtime} min</span>
+                       <span>{infoFilm.release_date}</span>
+                       <span>{infoFilm.runtime} min</span>
                     </div>
                     <p className="description">
-                        {info.overview}
+                        {infoFilm.overview}
                     </p>
                     <div className="genres-and-production-countries">
-                        <p>Genres: {info.genres ? info.genres[0].name : ''}</p>
-                        <p>Production countries: {info.production_countries
-                            ? info.production_countries[0].name
-                            : ''}
-                        </p>
+                        <p>Genres: {infoFilm.genres}</p>
+                        <p>Production countries: {infoFilm.production_countrie}</p>
                     </div>
                 </div>
             </div>
@@ -56,7 +84,8 @@ class MoveDescription extends React.Component{
 const mapStateToProps = function(store) {
     return {
         filmID: store.storeDetailFilm.filmID,
-        info: store.storeDetailFilm.filmUnique
+        info: store.storeDetailFilm.filmUnique,
+        searchType: store.storeFilms.searchType
     };
 };
 
